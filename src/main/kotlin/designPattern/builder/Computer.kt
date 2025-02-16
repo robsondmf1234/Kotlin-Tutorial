7cbe73efe42395dae271e53578636907d03c9c6f
robsondmf <robsondmf1234@gmail.com>
2025-02-16 11:16:37 -0300

    feat - desgin patterns.

---
+++ src/main/kotlin/designPattern/builder/Computer.kt
@@ -0,0 +1,11 @@
+package org.example.designPattern.builder
+
+// Definição da classe Computer com propriedades privadas
+data class Computer(
+    private val cpu: String,
+    private val ram: String,
+    private val storage: String,
+    private val gpu: String,
+    private val os: String,
+    private val motherboard: String?
+)
---
+++ src/main/kotlin/designPattern/strategy/MainStrategy.kt
@@ -0,0 +1,37 @@
+package designPattern.strategy
+
+
+//fun main() {
+//    // Criação de uma nova instância de FazerFrete
+//    val delivery = FazerFrete()
+//
+//    // Loop de 0 a 30
+//    for (i in 0..30) {
+//        // Seleção aleatória de uma prioridade
+//        var prioridade: Priority = Priority.values().random()
+//        // Seleção aleatória de um tipo de transporte
+//        val random = (0..2).random()
+//        // Dependendo do valor aleatório, um tipo de transporte é selecionado e uma prioridade é definida
+//        when (random) {
+//            0 -> {
+//                // Se o valor aleatório for 0, o transporte rodoviário é selecionado e a prioridade é definida como baixa
+//                delivery.transporte = Rodoviario()
+//                prioridade = Priority.LOW
+//            }
+//
+//            1 -> {
+//                // Se o valor aleatório for 1, o transporte marítimo é selecionado e a prioridade é definida como média
+//                delivery.transporte = Maritimo()
+//                prioridade = Priority.MEDIUM
+//            }
+//
+//            2 -> {
+//                // Se o valor aleatório for 2, o transporte aéreo é selecionado e a prioridade é definida como alta
+//                delivery.transporte = Aereo()
+//                prioridade = Priority.HIGH
+//            }
+//        }
+//        // O frete é realizado com a prioridade definida
+//        delivery.fazerFrete(prioridade)
+//    }
+//}
