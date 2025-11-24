37ca731d99efcc08af524428c2d61ea23da9a8c3
Robson <robsondmf1234@gmail.com>
2025-11-24 12:06:48 -0300

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,17 @@
 plugins {
     kotlin("jvm") version "1.9.22"
+    application
 }
-
-group = "org.example"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
 kotlin {
-    jvmToolchain(8)
+    jvmToolchain(11)   // 👈 Ajuste importante
 }
